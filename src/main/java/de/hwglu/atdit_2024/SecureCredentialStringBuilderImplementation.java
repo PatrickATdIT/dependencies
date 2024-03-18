3895ee847addb40901502ae9943e933a33862385
PatrickATdIT <atdit@wurstbox.com>
2024-03-18 08:33:15 +0100

    Dependency Injection, Manual Implementation

--- src/main/java/de/hwglu/atdit_2024/SecureCredentialStringBuilderImplementation.java
+++ src/main/java/de/hwglu/atdit_2024/SecureCredentialStringBuilderImplementation.java
@@ -1,13 +1,18 @@
 package de.hwglu.atdit_2024;
 
 public class SecureCredentialStringBuilderImplementation implements CredentialStringBuilder {
+    private final Encryption encryptor;
+
+    public SecureCredentialStringBuilderImplementation(Encryption encryptor) {
+        this.encryptor = encryptor;
+    }
+
     @Override
     public String makeCredentialString(String user, String password) {
         StringBuilder sb = new StringBuilder();
-        Encryption encrypter = new Caesar3Encryption();
 
-        String encryptedUser = encrypter.encrypt(user);
-        String encryptedPassword = encrypter.encrypt(password);
+        String encryptedUser = encryptor.encrypt(user);
+        String encryptedPassword = encryptor.encrypt(password);
 
         sb.append(encryptedUser)
                 .append(":")
--- src/test/java/de/hwglu/atdit_2024/SecureCredentialStringBuilderImplementationTest.java
+++ src/test/java/de/hwglu/atdit_2024/SecureCredentialStringBuilderImplementationTest.java
@@ -3,13 +3,23 @@ package de.hwglu.atdit_2024;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Objects;
+
 public class SecureCredentialStringBuilderImplementationTest {
+
+
     @Test
     public void userAndPasswordMustBeSeparatedByColon() {
+        Encryption encryptorMock = plain -> {
+            if (!Objects.equals(plain, "myuser") && !Objects.equals(plain, "mypassword"))
+                Assertions.fail("unexpected input");
+            return plain;
+        };
+
         String inputUser = "myuser";
         String inputPassword = "mypassword";
-        String expected = "pbxvhu:pbsdvvzrug";
-        var cut = new SecureCredentialStringBuilderImplementation();
+        String expected = "myuser:mypassword";
+        var cut = new SecureCredentialStringBuilderImplementation(encryptorMock);
 
         var actual = cut.makeCredentialString(inputUser, inputPassword);
 
