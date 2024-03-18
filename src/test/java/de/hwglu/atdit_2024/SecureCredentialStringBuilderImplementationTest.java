290fbf115df524751eb014f0117f14b1490fa1ff
PatrickATdIT <atdit@wurstbox.com>
2024-03-18 09:30:00 +0100

    Mockito

--- src/test/java/de/hwglu/atdit_2024/SecureCredentialStringBuilderImplementationTest.java
+++ src/test/java/de/hwglu/atdit_2024/SecureCredentialStringBuilderImplementationTest.java
@@ -2,27 +2,32 @@ package de.hwglu.atdit_2024;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import java.util.Objects;
+import org.mockito.Mockito;
 
 public class SecureCredentialStringBuilderImplementationTest {
 
 
     @Test
     public void userAndPasswordMustBeSeparatedByColon() {
-        Encryption encryptorMock = plain -> {
-            if (!Objects.equals(plain, "myuser") && !Objects.equals(plain, "mypassword"))
-                Assertions.fail("unexpected input");
-            return plain;
-        };
-
+        //assemble
         String inputUser = "myuser";
         String inputPassword = "mypassword";
         String expected = "myuser:mypassword";
+
+        //create mock and tell it how to behave!
+        Encryption encryptorMock = Mockito.mock(Encryption.class);
+        Mockito.when(encryptorMock.encrypt(inputUser)).thenReturn(inputUser);
+        Mockito.when(encryptorMock.encrypt(inputPassword)).thenReturn(inputPassword);
+
         var cut = new SecureCredentialStringBuilderImplementation(encryptorMock);
 
+        //act
         var actual = cut.makeCredentialString(inputUser, inputPassword);
 
+        //assert
+        //confirm that the encryptor was called as expected
+        Mockito.verify(encryptorMock, Mockito.times(1)).encrypt(inputUser);
+        Mockito.verify(encryptorMock, Mockito.times(1)).encrypt(inputPassword);
         Assertions.assertEquals(expected, actual);
     }
 }
